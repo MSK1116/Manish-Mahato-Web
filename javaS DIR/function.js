a8a9cbe2072d2555de28a4618cd59b4b9170776f
MSK1116 <anishkushwaha1116@gmail.com>
2023-10-19 08:26:54 +0545

    push

--- javaS DIR/function.js
+++ javaS DIR/function.js
@@ -1,16 +1,1 @@
-var a = new Date();
-var c = a.getHours();
 
-console.log(c);
-$(document).ready(function () {
-  if (c > 23 || (c >= 4 && c < 11)) {
-    $("#greet").text("It's mid night, better to sleep now!");
-  } else if (c > 4 && c < 11) {
-    $("#greet").text("Morning");
-  } else if (c > 12 && c < 18) {
-    $("#greet").text("Good Evening");
-  } else {
-    console.log("sex3");
-    $("#greet").text("Namaste");
-  }
-});
