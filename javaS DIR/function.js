d794a2dcee004f5250c172c63fe6b305d48e6d31
MSK1116 <anishkushwaha1116@gmail.com>
2023-10-21 13:11:41 +0545

    push

--- javaS DIR/function.js
+++ javaS DIR/function.js
@@ -1,1 +1,6 @@
+// Get the viewport width and height
+var vhW = window.innerWidth || document.documentElement.clientWidth;
+var vhH = window.innerHeight || document.documentElement.clientHeight;
 
+console.log("Viewport Width: " + vhW + " pixels");
+console.log("Viewport Height: " + vhH + " pixels");
--- javaS DIR/react.js
+++ javaS DIR/react.js
@@ -1,49 +1,74 @@
 // variable
 var x = false;
-$(document).ready(function () {
-  let scrollPos = 0;
-  $(window).scroll(function () {
-    var newScrollPos = $(window).scrollTop();
-    if (newScrollPos > scrollPos) {
-      $("nav").slideUp(900);
-      $(".nav-item").slideUp(1000);
-      x = true;
-      down();
-      console.log(x);
-    }
-    if (newScrollPos < scrollPos) {
-      $("nav").slideDown();
-      $(".nav-item").slideDown();
-      x = false;
-      down();
-    }
-    scrollPos = newScrollPos;
+var vhW = window.innerWidth || document.documentElement.clientWidth;
+var vhH = window.innerHeight || document.documentElement.clientHeight;
+if (vhW > 655) {
+  console.log("Device scrren was found to be in ggod condition, Loading all the animation...");
+  $(document).ready(function () {
+    let scrollPos = 0;
+    $(window).scroll(function () {
+      var newScrollPos = $(window).scrollTop();
+      if (newScrollPos > scrollPos) {
+        $("nav").slideUp(900);
+        $(".nav-item").slideUp(1000);
+        x = true;
+        down();
+        console.log(x);
+      }
+      if (newScrollPos < scrollPos) {
+        $("nav").slideDown();
+        $(".nav-item").slideDown();
+        x = false;
+        down();
+      }
+      scrollPos = newScrollPos;
+    });
   });
-});
 
-$("#nav-up").on("click", function () {
-  $("nav").slideUp(1000);
-  $(".nav-item").slideUp(1000);
-  x = true;
-  down();
-  console.log(x);
-});
+  $("#nav-up").on("click", function () {
+    $("nav").slideUp(1000);
+    $(".nav-item").slideUp(1000);
+    x = true;
+    down();
+    console.log(x);
+  });
 
-$("#nav-down").css("display", "none");
+  $("#nav-down").css("display", "none");
 
-function down() {
-  if (x == true) {
-    console.log("activated");
-    $("#nav-down").fadeIn(200); // Added this line to display "nav-down"
+  function down() {
+    if (x == true) {
+      console.log("activated");
+      $("#nav-down").fadeIn(200); // Added this line to display "nav-down"
+      $("#nav-down").on("click", function () {
+        $("nav").slideDown();
+        $(".nav-item").slideDown();
+        $("#nav-down").fadeOut(500);
+      });
+    } else {
+      console.log("Message form navbar is that, is loaded");
+      $("#nav-down").fadeOut(500);
+    }
+  }
+} else {
+  console.log("Device was found with small scrren, Remove the animation");
+  var x = false;
+  if (x == false) {
+    $("#nav-down").fadeIn(200);
     $("#nav-down").on("click", function () {
       $("nav").slideDown();
       $(".nav-item").slideDown();
       $("#nav-down").fadeOut(500);
+      x = true;
     });
-  } else {
-    console.log("Message form navbar is that, is is loaded");
-    $("#nav-down").fadeOut(500);
   }
+  $(".content").on("click", function () {
+    if (x == true) {
+      $("#nav-down").fadeIn(200);
+      $("nav").slideUp(900);
+      $(".nav-item").slideUp(1000);
+      x = false;
+    }
+  });
 }
 
 $("#scroll1").on("click", function () {
